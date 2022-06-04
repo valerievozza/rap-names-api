a52028516323f9ed8bedf28ea49180744441a95d
valerievozza <valerievozza@gmail.com>
2022-06-04 11:24:50 -0700

    added heroku port

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get('/api/:name', (request, response) => {
     }
 })
 
-app.listen(PORT, () => {
-    console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is now running on port ${PORT}!`)
 })
